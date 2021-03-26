c122a360f7893a9e8495b70f21247ab8bc5f172c
Bogdan <bogdan.talpiga@3nyble.com>
2021-03-26 17:16:53 +0200

    Change pom package mode

--- src/test/java/JsonTest.java
+++ src/test/java/JsonTest.java
@@ -1,9 +1,16 @@
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.nyble.App;
 import com.nyble.api.messages.ConsumerInfoResponse;
 import com.nyble.api.messages.ConsumerInfoResponseAdapter;
+import com.nyble.main.ComparatorUtils;
+import com.nyble.topics.Names;
+import com.nyble.topics.consumerAttributes.ConsumerAttributesKey;
+import com.nyble.topics.consumerAttributes.ConsumerAttributesValue;
+import com.nyble.utils.StringConverter;
 import junit.framework.TestCase;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class JsonTest extends TestCase {
 
@@ -33,6 +40,29 @@ public class JsonTest extends TestCase {
         assertEquals("duru", rsp.consumers[0].getLastname());
         assertEquals("ion", rsp.consumers[1].getFirstname());
         assertEquals("ionescu", rsp.consumers[1].getLastname());
+    }
+
+    public void test_cornerCaseName(){
+        String firstName = new StringConverter("DANIE;A").trim().nullIf("").get();
+        firstName = new ComparatorUtils(firstName).convertToNull().toUpper().replaceDiacritics()
+                .removeExtraCharsAndDuplicates().removeWhiteSpaces()
+                .getText();
+
+        String lastName = new StringConverter("TOMA").trim().nullIf("").get();
+        lastName = new ComparatorUtils(lastName).convertToNull().toUpper().replaceDiacritics()
+                .removeExtraCharsAndDuplicates().removeWhiteSpaces()
+                .getText();
+
+        String expected = "DANIE;ATOMA";
+        String actual = ComparatorUtils.getFullName(firstName, lastName);
+        System.out.println(actual);
+        assertEquals(expected, actual);
+
+        String value = new StringConverter(actual).coalesce("").get();
+        ConsumerAttributesValue cav = new ConsumerAttributesValue(0 + "", 0 + "", "fullName",
+                value, 0+"", 0+"");
+        String attributeJson = cav.toJson();
+        System.out.println(attributeJson);
     }
 
 }
